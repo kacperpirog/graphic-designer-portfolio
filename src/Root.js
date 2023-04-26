1004858a51354dab58c8933faab0603c4ed2b789
Kacper Pirog <kacper.pirog@gmail.com>
2023-04-26 14:04:13 +0200

    add components

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,20 @@
 import React from "react";
+import AboutMe from "./views/aboutMe";
+import SliderHome from "./components/slider";
+import Portfolio from "./views/portfolio";
+import Logos from "./components/logos";
+import Footer from "./components/footer";
 
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <div>
+      <SliderHome />
+      <AboutMe />
+      <Portfolio />
+      <Logos />
+      <Footer />
+    </div>
+  );
 };
 
 export default Root;
---
+++ src/components/nav/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Works</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
