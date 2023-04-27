22d1d976f6942ecb023945238e92b1664cd4c274
Kacper Pirog <kacper.pirog@gmail.com>
2023-04-27 14:06:27 +0200

    styled page AboutMe

--- src/components/aboutMeWrapper/index.js
+++ src/components/aboutMeWrapper/index.js
@@ -3,12 +3,20 @@ import "../../globalStyled/partials/_aboutMe.css";
 
 const AboutMeWrapper = () => {
   return (
-    <div className="aboutMeConteiner">
-      <div>nav</div>
-      <div>
+    <div className="aboutMeWrapper">
+      {/* <div className="webGraphic"> </div> */}
+      <div className="aboutMeConteiner">
         <div className="fotoProfile"> </div>
-        <div>
-          Kim jestem <h3>Witam,</h3>
+        <div className="aboutMeCopy">
+          <h2>Kim jestem</h2> <h3>Witam,</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+            facilis adipisci accusantium repellendus temporibus quis quidem.
+            Doloremque eligendi vitae optio culpa illo nobis. Porro nam quae
+            iusto non suscipit ab excepturi inventore, assumenda facilis ea.
+            Similique eos sit, a corrupti perferendis accusantium nam totam fuga
+            at ipsum voluptate cumque dolores.
+          </p>
         </div>
       </div>
     </div>
