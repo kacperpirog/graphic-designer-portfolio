0e750db148d08d66da3f0c0cc4f312ec634c8e5f
Kacper Pirog <kacper.pirog@gmail.com>
2023-04-28 22:21:55 +0200

    add logos _logoCarousel

--- src/components/logos/index.js
+++ src/components/logos/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "../../globalStyled/partials/_logoCarousel.css";
 
 const Logos = () => {
-  return <div>Logos</div>;
+  return (
+    <>
+      <div className="slider">
+        <div className="slide-track2">
+          <div className="slide bg-red-500">1</div>
+          <div className="slide bg-purple-500">2</div>
+          <div className="slide bg-blue-500">3</div>
+          <div className="slide bg-green-500">4</div>
+          <div className="slide bg-pink-500">5</div>
+
+          <div className="slide bg-red-500">1</div>
+          <div className="slide bg-purple-500">2</div>
+          <div className="slide bg-blue-500">3</div>
+          <div className="slide bg-green-500">4</div>
+          <div className="slide bg-pink-500">5</div>
+
+          <div className="slide bg-red-500">1</div>
+          <div className="slide bg-purple-500">2</div>
+          <div className="slide bg-blue-500">3</div>
+          <div className="slide bg-green-500">4</div>
+          <div className="slide bg-pink-500">5</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Logos;
