85a5f14638d6448ad0ddc16791da1573b706d292
Kacper Pirog <kacper.pirog@gmail.com>
2023-04-28 22:46:40 +0200

    fix beta logos _logoCarousel

--- src/components/logos/index.js
+++ src/components/logos/index.js
@@ -1,28 +1,36 @@
 import React from "react";
 import "../../globalStyled/partials/_logoCarousel.css";
+import uaLogo from "../../assets/logos/UA.png";
 
 const Logos = () => {
   return (
     <>
       <div className="slider">
         <div className="slide-track2">
-          <div className="slide bg-red-500">1</div>
-          <div className="slide bg-purple-500">2</div>
-          <div className="slide bg-blue-500">3</div>
-          <div className="slide bg-green-500">4</div>
-          <div className="slide bg-pink-500">5</div>
-
-          <div className="slide bg-red-500">1</div>
-          <div className="slide bg-purple-500">2</div>
-          <div className="slide bg-blue-500">3</div>
-          <div className="slide bg-green-500">4</div>
-          <div className="slide bg-pink-500">5</div>
-
-          <div className="slide bg-red-500">1</div>
-          <div className="slide bg-purple-500">2</div>
-          <div className="slide bg-blue-500">3</div>
-          <div className="slide bg-green-500">4</div>
-          <div className="slide bg-pink-500">5</div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
+          <div className="slide ">
+            <img src={uaLogo} alt="University of Arizona" />
+          </div>
         </div>
       </div>
     </>
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -45,12 +45,7 @@ const SliderHome = () => {
           <div class="ball"></div>
         </div>
       </div>
-      <div className="sliderConteiner">
-        <div className="sliderFoto01"></div>
-        <div className="sliderFoto02"></div>
-        <div className="sliderFoto03"></div>
-        <div className="sliderFoto04"></div>
-      </div>
+      <div className="sliderConteiner"></div>
       <div className="sliderInfo">
         <h1>Lorem </h1>
         <h2>Lorem ipsumn</h2>
