b00842edcae8b407dde1c7474d0c20d291899dee
Kacper Pirog <kacper.pirog@gmail.com>
2023-05-01 22:02:52 +0200

    add and styled footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../../globalStyled/partials/_footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footerDiv">
+      <p>Copyright &copy; {new Date().getFullYear()} Kacper Piróg</p>
+    </div>
+  );
 };
 
 export default Footer;
