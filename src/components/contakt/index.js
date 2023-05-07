fb76c680ad180ced4ca0317c44ebb8dafd9dd596
Kacper Pirog <kacper.pirog@gmail.com>
2023-05-07 16:39:05 +0200

    add icons to contact

--- src/components/contakt/index.js
+++ src/components/contakt/index.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../globalStyled/partials/_contact.css";
-
 import { Element } from "react-scroll";
 import profileFoto from "../../assets/profile/foto_KP.jpeg";
+import linkedin from "../../assets/profile/linkedin.svg";
+import bithub from "../../assets/profile/square-github.svg";
+import cv from "../../assets/profile/cv.svg";
+import cv_01 from "../../assets/profile/Kacper_Piróg_CV_Graphic_pl.jpg";
 
 const ContactForm = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
     <>
       <Element name="contact" className="element">
         <div className="contactFormWrapper">
-          <div>
+          <div className="imgStyled">
             <img src={profileFoto} alt="foto" />
             <p>Kacper Piróg</p>
           </div>
 
           <div>
-            <h1>Kacper Piróg</h1>
-            <h2>
-              Kraków,
-              <br />
-              Poland
-            </h2>
+            <a href="https://www.linkedin.com/in/kacper-pir%C3%B3g-0991a151">
+              <img className="styledSVG" src={linkedin} alt="linkedin" />
+            </a>
+
+            <a href="https://github.com/kacperpirog">
+              <img className="styledSVG" src={bithub} alt="bithub" />
+            </a>
+            <a href={cv_01}>
+              <img className="styledSVG" src={cv} alt="cv" />
+            </a>
           </div>
 
           <div className="rightCopy">
