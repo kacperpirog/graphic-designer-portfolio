62966b5bd625facb71b589546bf7f4e126f84400
Kacper Pirog <kacper.pirog@gmail.com>
2023-05-07 20:56:29 +0200

    add icons to slider

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../../globalStyled/partials/_slider.css";
 import { Element } from "react-scroll";
+import linkedin from "../../assets/profile/linkedin.svg";
+import bithub from "../../assets/profile/square-github.svg";
+import cv from "../../assets/profile/cv.svg";
+import cv_01 from "../../assets/profile/Kacper_Piróg_CV_Graphic_pl.jpg";
 
 const SliderHome = () => {
   return (
@@ -51,7 +55,18 @@ const SliderHome = () => {
         <div className="sliderInfo">
           <h1>Kacper Piróg</h1>
           <h2>Graphic designer & Front-end</h2>
+          <div>
+            <a href="https://www.linkedin.com/in/kacper-pir%C3%B3g-0991a151">
+              <img className="styledSVG" src={linkedin} alt="linkedin" />
+            </a>
 
+            <a href="https://github.com/kacperpirog">
+              <img className="styledSVG" src={bithub} alt="bithub" />
+            </a>
+            <a href={cv_01}>
+              <img className="styledSVG" src={cv} alt="cv" />
+            </a>
+          </div>
           {/* <button>WORK</button> */}
         </div>
       </div>
